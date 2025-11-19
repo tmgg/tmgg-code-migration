65d115a16188826ff9f77d12264c8a74e6a636e8
jiangtao <jiangtao.cctv@qq.com>
2025-11-19 14:10:33 +0800

    init

--- src/main/java/io/github/tmgg/code/migration/Main.java
+++ src/main/java/io/github/tmgg/code/migration/Main.java
@@ -2,42 +2,51 @@ package io.github.tmgg.code.migration;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.lang.Assert;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.setting.Setting;
+import cn.hutool.setting.SettingUtil;
 import io.github.tmgg.code.migration.utils.ProjectUtils;
 
-import java.util.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
+
+    public static final String[] FILE_TYPES = {"java", "jsx"};
+
     public static void main(String[] args) {
-        System.out.println(ProjectUtils.getDir());
-        java();
-     //   pkg();
+        System.out.println(getDir());
 
+        for (String fileType : FILE_TYPES) {
+            Map<String, String> replaceMap = new LinkedHashMap<>();
 
-    }
-
-    private static void java() {
-        Map<String, String> replaceMap = new LinkedHashMap<>();
-
-        String s = ResourceUtil.readUtf8Str("replace-java.txt").trim();
-        List<String> lines = StrUtil.splitTrim(s, "\n");
-        for (int i = 0; i < lines.size(); i++) {
-            String key = lines.get(i);
-            String value = lines.get(++i);
-            replaceMap.put(key, value);
+            String s = ResourceUtil.readUtf8Str("replace-" + fileType + ".txt").trim();
+            List<String> lines = StrUtil.splitTrim(s, "\n");
+            for (int i = 0; i < lines.size(); i++) {
+                String key = lines.get(i);
+                String value = lines.get(++i);
+                replaceMap.put(key, value);
+            }
+           replace(fileType, replaceMap);
         }
 
-        ProjectUtils.replace(".java", replaceMap);
+        pkg();
     }
 
+
+
     private static void pkg() {
-        String file = ProjectUtils.getDir() + "/web/package.json";
+        String file = getDir() + "/web/package.json";
         List<String> lines = FileUtil.readUtf8Lines(file);
 
         List<String> newLines = new ArrayList<>();
         for (String line : lines) {
-            if (StrUtil.containsAny(line, "@types/react-dom", "@types/react","\"react\"","react-dom")
-            && line.contains("18.")) {
+            if (StrUtil.containsAny(line, "@types/react-dom", "@types/react", "\"react\"", "react-dom")
+                    && line.contains("18.")) {
                 String newLine = line.replaceAll("\\^18[^\\s,}\"]*", "^19.0.0");
                 System.err.println("react版本修改为19:" + line + "-->" + newLine);
                 newLines.add(newLine);
@@ -48,9 +57,37 @@ public class Main {
         FileUtil.writeUtf8Lines(newLines, file);
 
         String content = FileUtil.readUtf8String(file);
-        if(!content.contains("@ant-design/v5-patch-for-react")){
+        if (!content.contains("@ant-design/v5-patch-for-react")) {
             // "@ant-design/v5-patch-for-react-19": "^1.0.3"
             throw new IllegalStateException("请手动添加 \"@ant-design/v5-patch-for-react-19\": \"^1.0.3\"");
         }
     }
+    public static File getDir() {
+        Setting setting = SettingUtil.get("config");
+        String dir = setting.get("dir");
+        File file = new File(dir);
+        Assert.state(file.exists(), "目录不存在");
+        return file;
+    }
+
+
+    public static void replace(String fileType, Map<String,String> replaceMap) {
+        File dir = getDir();
+
+        List<File> files = FileUtil.loopFiles(dir, pathname -> pathname.getName().endsWith(fileType));
+
+        for (File file : files) {
+            String content = FileUtil.readUtf8String(file);
+            String newContent = content;
+            for (Map.Entry<String, String> e : replaceMap.entrySet()) {
+                newContent = StrUtil.replace(newContent, e.getKey(), e.getValue());
+            }
+
+            if(!content.equals(newContent)){
+                System.out.println("修改文件 " + file.getName());
+                FileUtil.writeUtf8String(newContent,file);
+            }
+        }
+    }
+
 }
