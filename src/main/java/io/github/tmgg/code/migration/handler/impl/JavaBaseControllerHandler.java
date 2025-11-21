53d5ab64b80e64e5666973d0879366c13c4eed71
jiangtao <jiangtao.cctv@qq.com>
2025-11-21 15:03:30 +0800

    init

--- src/main/java/io/github/tmgg/code/migration/handler/impl/JavaBaseControllerHandler.java
+++ src/main/java/io/github/tmgg/code/migration/handler/impl/JavaBaseControllerHandler.java
@@ -1,8 +1,13 @@
 package io.github.tmgg.code.migration.handler.impl;
 
+import cn.hutool.core.text.finder.PatternFinder;
+import cn.hutool.core.text.finder.StrFinder;
+import cn.hutool.core.util.ReUtil;
+import cn.hutool.core.util.StrUtil;
 import io.github.tmgg.code.migration.handler.JavaHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.PatternMatchUtils;
 
 import java.util.List;
 
@@ -14,39 +19,49 @@ public class JavaBaseControllerHandler extends JavaHandler {
     @Override
     public String process(List<String> lines) {
         for (String line : lines) {
-            if(line.contains("BaseController")){
-                log.error("不应包含BaseController,请适用如下代码替换");
+            if(line.contains("extends BaseController<")){
+                log.error(line);
+                log.error("不应包含BaseController");
+
+                String entityName = ReUtil.getGroup1("<([^<>]+)>", line);
+                log.info("解析实体 {}",entityName);
+
+                String entityNameLowerFirst = StrUtil.lowerFirst(entityName);
+                log.info("小写开头 {}",entityNameLowerFirst);
+
+
 
                 String code = """
-                        @HasPermission("【替换】:view")
-                        @RequestMapping("page")
-                        public AjaxResult page(   @PageableDefault(direction = Sort.Direction.DESC, sort = "updateTime") Pageable pageable) throws Exception {
-                            JpaQuery<T> q = new JpaQuery<>();
-                                        
-                                        
-                            Page<T> page = service.pageByRequest(q, pageable);
-                                        
-                                        
-                                        
-                            return AjaxResult.ok().data(page);
-                        }
-                                        
-                        @HasPermission("【替换】:save")      
-                        @PostMapping("save")
-                        public AjaxResult save(@RequestBody T input, RequestBodyKeys updateFields) throws Exception {
-                            service.saveOrUpdateByRequest(input, updateFields);
-                            return AjaxResult.ok().msg("保存成功");
-                        }
-                                        
-                        @HasPermission("【替换】:delete")         
-                        @RequestMapping("delete")
-                        public AjaxResult delete(String id) {
-                            service.deleteByRequest(id);
-                            return AjaxResult.ok().msg("删除成功");
-                        }
-                    """;
+                            @HasPermission("【替换小】:view")
+                            @RequestMapping("page")
+                            public AjaxResult page(【替换大】 request,@PageableDefault(direction = Sort.Direction.DESC, sort = "updateTime") Pageable pageable) throws Exception {
+                                JpaQuery<【替换大】> q = new JpaQuery<>();
+                                // 视情况修改
+                                q.likeExample(request);
+                                Page<【替换大】> page = service.pageByRequest(q, pageable);
+                                return AjaxResult.ok().data(page);
+                            }
+                                            
+                            @HasPermission("【替换小】:save")      
+                            @PostMapping("save")
+                            public AjaxResult save(@RequestBody 【替换大】 input, RequestBodyKeys updateFields) throws Exception {
+                                service.saveOrUpdateByRequest(input, updateFields);
+                                return AjaxResult.ok().msg("保存成功");
+                            }
+                                            
+                            @HasPermission("【替换小】:delete")         
+                            @RequestMapping("delete")
+                            public AjaxResult delete(String id) {
+                                service.deleteByRequest(id);
+                                return AjaxResult.ok().msg("删除成功");
+                            }
+                        """;
 
-                System.out.println( code);
+                code = code.replace("【替换小】",entityNameLowerFirst);
+                code = code.replace("【替换大】",entityName);
+
+                log.info("1.删除 extends BaseController");
+                log.info("2.增加如下内容\n{}",code);
 
                 throw new IllegalStateException();
             }
